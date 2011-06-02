db153fe08adbd7d6be25411add903e8797ce37b5
atomaka <atomaka@gmail.com>
2011-06-02 04:32:58 -0400

    Reverted back to version hooking slash commands instead of events for managing stored data.

--- ChatSaver.lua
+++ ChatSaver.lua
@@ -1,6 +1,7 @@
 ChatSaver = LibStub('AceAddon-3.0'):NewAddon('ChatSaver','AceConsole-3.0','AceHook-3.0','AceEvent-3.0');
 local core = ChatSaver;
 
+--reverting
 function core:OnInitialize()	
 	self:RegisterChatCommand('cs','SlashCommand');
 	
